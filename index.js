3a885b00f86be2b78c5e8253aace643d8c79a535
Arkokhan21 <khanarko2021@gmail.com>
2022-10-25 17:17:08 +0600

    added dynamic api

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const courses = require('./data/courses.json')
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Server running')
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id
+    const selectedcourse = courses.find(course => course.id === id)
+    res.send(selectedcourse)
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Server side running in this port ${port}`)
 })
